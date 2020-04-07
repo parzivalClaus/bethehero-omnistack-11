cea8e5704e320f2562eaad7e1cc81caa532c5ad2
parzivalClaus <claudio@kaizenfotoprodutos.com.br>
2020-04-07 15:22:58 -0300

    Migrations e primeira rota

---
+++ backend/src/routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const routes = express.Router();
+
+routes.post("/ongs", (req, res) => {
+  const data = req.body;
+
+  console.log(data);
+
+  return res.json();
+});
+
+module.exports = routes;
