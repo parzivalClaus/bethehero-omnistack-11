7bfa2f08c3ce4a6e2ff9fdccc5fe85d679aa2a25
parzivalClaus <claudio@kaizenfotoprodutos.com.br>
2020-04-07 13:55:21 -0300

    Conhecendo a Omnistack - Finalizado

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 const express = require("express");
 
 const app = express();
+
+app.get("/", (req, res) => {
+  return res.json({
+    event: "Semana Omnistack 11.0",
+    aluno: "Claudio Ferreira",
+  });
+});
+
+app.listen(3333);
