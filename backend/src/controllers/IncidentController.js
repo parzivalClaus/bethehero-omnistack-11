a499a9c29e9f0b4a64053404e13365a2a98a45e3
parzivalClaus <claudio@kaizenfotoprodutos.com.br>
2020-04-07 18:46:17 -0300

    Criando a base da aplicaçao - Finalizado

---
+++ backend/src/controllers/IncidentController.js
@@ -0,0 +1,67 @@
+const connection = require("../database/connection");
+
+module.exports = {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const [count] = await connection("incidents").count();
+
+    const incidents = await connection("incidents")
+      .join("ongs", "ongs.id", "=", "incidents.ong_id")
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        "incidents.*",
+        "ongs.name",
+        "ongs.email",
+        "ongs.whatsapp",
+        "ongs.city",
+        "ongs.uf",
+      ]);
+
+    res.header("X-Total-Count", count["count(*)"]);
+
+    return res.json(incidents);
+  },
+
+  async create(req, res) {
+    const { title, description, value } = req.body;
+    const ong_id = req.headers.authorization;
+
+    const [id] = await connection("incidents").insert({
+      title,
+      description,
+      value,
+      ong_id,
+    });
+
+    return res.json({ id });
+  },
+
+  async delete(req, res) {
+    const { id } = req.params;
+    const ong_id = req.headers.authorization;
+
+    const incident = await connection("incidents")
+      .where("id", id)
+      .select("ong_id")
+      .first();
+
+    if (!incident) {
+      return res
+        .status(400)
+        .json({ error: "Este caso já foi apagado ou nunca existiu" });
+    }
+
+    if (incident.ong_id !== ong_id) {
+      return res.status(401).json({
+        error:
+          "Essa operação não é permitida. O caso foi criado por uma ONG diferente.",
+      });
+    }
+
+    await connection("incidents").where("id", id).delete();
+
+    return res.status(204).send();
+  },
+};
